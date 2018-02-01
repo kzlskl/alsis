f73cf2bacfd16d31350d4b99aab4767d095dd543
kzlskl <iybayazit@hotmail.com>
2018-02-01 13:41:47 +0300

    Improvements/fixes
    fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # herakles
+Made for just personel purposes.
+If you want to use it, you better look into script.
--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 echo "Arch Linux Basic Installer Script for Personel Purposes (Lenovo x200)"
 
+mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+
 pacman -Syyu
 pacman -S - < programs.both
 
@@ -13,3 +16,5 @@ systemctl mask systemd-rfkill.service systemd-rfkill.socket
 echo -n "Username:"
 read name
 useradd -m -g users -G optical,storage,wheel,video,audio,users,power,network,log -s /bin/bash $name
+
+echo "programlar yüklendi, linux-lts'ye geçildi, tlp kuruldu, kullanıcı oluşturuldu."
