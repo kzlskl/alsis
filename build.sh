778c046f159f452c72801fc9bcd68efc93c59dbc
kzlskl <iybayazit@hotmail.com>
2018-02-01 13:20:12 +0300

    Improvements
    Trying to use scripting

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Arch Linux Basic Installer Script for Personel Purposes (Lenovo x200)"
+
+pacman -Syyu
+pacman -S - < programs.both
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable lightdm.service tlp.service tlp-sleep.service NetworkManager-dispatcher.service
+systemctl mask systemd-rfkill.service systemd-rfkill.socket
+
+echo -n "Username:"
+read name
+useradd -m -g users -G optical,storage,wheel,video,audio,users,power,network,log -s /bin/bash $name
--- programs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Echo "Arch Linux Basic Program Installer Script for Personel Purposes (Lenovo x200)"
-
-pacman -S xfce4 xfce4-goodies vlc audacious firefox firefox-i18n-tr thunderbird thunderbird-i18n-tr pavucontrol network-manager-applet ntfs-3g engrampa unrar unzip zip p7zip exfat-utils lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker cups cups-pdf system-config-printer print-manager foomatic-db foomatic-db-nonfree-ppds foomatic-db-ppds arc-gtk-theme numix-gtk-theme pulseaudio pulseaudio-equalizer audacity gtk-recordmydesktop soundconverter gnome-disk-utility evince transmission-gtk pidgin libreoffice-still libreoffice-still-tr gparted dconf-editor thunar-archive-plugin gvfs intel-ucode xf86-video-intel
-
-systemctl enable lightdm.service
