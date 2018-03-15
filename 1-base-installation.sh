98eb8356a2ddeef43dc0cbfea9a6516debaef501
kzlskl <iybayazit@hotmail.com>
2018-03-16 00:20:28 +0300

    trying somethink

--- 1-base-installation.sh
+++ 1-base-installation.sh
@@ -33,7 +33,22 @@ mount /dev/$bootpart /mnt/boot
 swapon /dev/$swappart
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist &
+spinner()
+{
+    local pid=$!
+    local delay=0.75
+    local spinstr='...'
+    echo "This may take 10 minutes "
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf "%s  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
 
 pacstrap -i /mnt base base-devel grub
 genfstab -L -p /mnt >> /mnt/etc/fstab
